25ed96e3dc50db96883d977c1fee6b6f4fd5303d
tlouisebaa <gadorabegail24@gmail.com>
2022-12-13 21:00:38 +0800

    added login

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,6 +50,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'CCRMS/index';
+
+// Login
+$route['login']['GET'] = 'CCRMS';
+$route['login']['POST'] = 'CCRMS/login';
+// $route['logout']['GET'] = 'Logout/logout';
+
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 $route['forgot'] = 'CCRMS/forgot';
